647107f53e933f4f847d734031fa9ce44374ee02
Kizuren <96580944+Kizuren@users.noreply.github.com>
2025-02-25 00:13:49 +0100

    Add PORT configuration to environment and update server to use it

--- server.py
+++ server.py
@@ -179,4 +179,5 @@ def delete_link(short_id):
     return jsonify({'success': True})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
